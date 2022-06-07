93121da8348d8a502625ec13d778c9c66f37780f
Adam Hicks <adam@luno.com>
2022-06-07 11:55:27 +0100

    server: Include metrics.go

---
+++ server/handlers/metrics.go
@@ -0,0 +1,16 @@
+package handlers
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	httpHandle = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "gridlock",
+		Subsystem: "server",
+		Name:      "http_handled_seconds",
+		Help:      "Handled HTTP request latency",
+	}, []string{"path"})
+)
+
+func init() {
+	prometheus.MustRegister(httpHandle)
+}
